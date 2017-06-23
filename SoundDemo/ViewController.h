e6b1d2128db8c9955248a36d7156be1752b78ba0
Gu Han <guhan@Gus-MacBook-Pro.local>
2017-06-22 20:43:22 -0700

    Initial Commit

---
+++ SoundDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundDemo
+//
+//  Created by Gu Han on 6/22/17.
+//  Copyright © 2017 Gu Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SoundDemo
+//
+//  Created by Gu Han on 6/22/17.
+//  Copyright © 2017 Gu Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
